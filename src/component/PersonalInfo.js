fed6d6e98f1b7566fedc5f1c66096c9665df6b61
Peter <petervsjim@gmail.com>
2022-01-26 15:56:42 +0800

    implement person info

--- src/component/PersonalInfo.js
+++ src/component/PersonalInfo.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import reactIcon from "../images/react.svg";
 import javascriptIcon from "../images/javascript.png";
 import cssIcon from "../images/css.svg";
@@ -11,27 +11,47 @@ import indesignIcon from "../images/indesign.svg";
 
 export default function PersonalInfo() {
   return (
-    <div className='personal-info-container'>
-    <h1 className='head-section'>個人簡介</h1>
-    <div className='personal-info-content-container'>
-      <div className='name title'>甘定中 Peter Kan</div>
-      <div className='introduction'>曾於日本公司實習，並具近一年產品經理經驗。</div>
-      <div className='subtitle programming-language'>程式語言</div>
-      <ul className='programming-language-images'>
-        <li><img className='icon' src={reactIcon} alt="" /></li>
-        <li><img className='icon' src={javascriptIcon} alt="" /></li>
-        <li><img className='icon' src={cssIcon} alt="" /></li>
-        <li><img className='icon' src={htmlIcon} alt="" /></li>
-        <li><img className='icon' src={javaIcon} alt="" /></li>
-        <li><img className='icon' src={pythonIcon} alt="" /></li>
-        <li><img className='icon' src={rubyIcon} alt="" /></li>
-      </ul>
-      <div className='subtitle other-tools'>其他工具</div>
-      <ul className='other-tools-images'>
-        <li><img className='icon' src={illustratorIcon} alt="" /></li>
-        <li><img className='icon' src={indesignIcon} alt="" /></li>
-      </ul>
-    </div>
+    <div className="personal-info">
+      <h1 className="head-section">個人簡介</h1>
+      <div className="personal-info-content">
+        <div className="name title">甘定中 Peter Kan</div>
+        <div className="introduction">
+          曾於日本網頁公司實習，並具近一年產品經理經驗。
+        </div>
+        <div className="subtitle programming-language">程式語言</div>
+        <ul className="icon-list programming-language-images">
+          <li>
+            <img className="icon" src={reactIcon} alt="" />
+          </li>
+          <li>
+            <img className="icon" src={javascriptIcon} alt="" />
+          </li>
+          <li>
+            <img className="icon" src={cssIcon} alt="" />
+          </li>
+          <li>
+            <img className="icon" src={htmlIcon} alt="" />
+          </li>
+          <li>
+            <img className="icon" src={javaIcon} alt="" />
+          </li>
+          <li>
+            <img className="icon" src={pythonIcon} alt="" />
+          </li>
+          <li>
+            <img className="icon" src={rubyIcon} alt="" />
+          </li>
+        </ul>
+        <div className="subtitle other-tools">其他工具</div>
+        <ul className="icon-list other-tools-images">
+          <li>
+            <img className="icon" src={illustratorIcon} alt="" />
+          </li>
+          <li>
+            <img className="icon" src={indesignIcon} alt="" />
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
